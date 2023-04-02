898f593193fa53091703aab30b1f5007a2687fbd
Anuja-y <anuja.yadwadkar@gmail.com>
2023-04-02 14:12:14 +0800

    final working copy with results

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,30 @@
 #import modules
 import os
 import csv
+
+# Create and initialize variables
 totalbudget = 0
 netchangelist = []
 averagechange = 0
 greatest_increase = ["", 0]
 greatest_decrease = ["", 9999999999999999999]
+Results = []
 
 # Import and Read budget_data.csv
 csvpath = os.path.join('PyBank\Resources','budget_data.csv')
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
-    #print(csvreader)
-    csv_header = next(csvreader)
-    #print(f'CSV Header:{csv_header}')
+    csv_header = next(csvreader) #skip header
     
     firstrow = next(csvreader)
     previousnet = int(firstrow[1]) 
     totalbudget = int(firstrow[1]) 
+
+    # Loop through csv file
     for row in csvreader:
-        #print(row)
-        #Capture data to calculate average change       
+        
+        #Capture data      
         totalbudget += int(row[1])
         netchange = int(row[1]) - previousnet
         netchangelist += [netchange]
@@ -36,9 +39,11 @@ with open(csvpath) as csvfile:
         if netchange < greatest_decrease[1]:
             greatest_decrease[0] = row[0]
             greatest_decrease[1] = netchange
+    
+    # Calculate Average Change
     averagechange = sum(netchangelist)/len(netchangelist)
 
-# Print results
+# Print results to terminal
 print("Financial Analysis")
 print("--------------------------------------------------------------")
 print("Total Months:", (csvreader.line_num - 1))
@@ -46,3 +51,15 @@ print("Total: $",totalbudget)
 print("Average Change: $",round((averagechange),2))
 print("Greatest Increase in Profits:", greatest_increase[0],"($",greatest_increase[1],")")
 print("Greatest Decrease in Profits:", greatest_decrease[0],"($",greatest_decrease[1],")")
+
+
+# Write results to a text file
+file = open("PyBank\Analysis\Results.txt","w")
+file.writelines(f'\n Financial Analysis')
+file.writelines(f"\n\n --------------------------------------------------------------")
+file.writelines(f"\n\n Total Months: {csvreader.line_num - 1}")
+file.writelines(f"\n\n Total: $ {totalbudget}")
+file.writelines(f"\n\n Average Change: ${round((averagechange),2)}")
+file.writelines(f"\n\n Greatest Increase in Profits: {greatest_increase[0]} ${greatest_increase[1]}")
+file.writelines(f"\n\n Greatest Decrease in Profits: {greatest_decrease[0]} ${greatest_decrease[1]}")
+file.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,26 +3,30 @@ import os
 import csv
 import operator as op
 
+# Create and initialise variables
 listofcandidates=[]
 listofvotes = []
 results = {}
 previousvotes = 0
+
 # Import and Read election_data.csv
 csvpath = os.path.join('PyPoll\Resources','election_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    #print(csvreader)
     csv_header = next(csvreader)
-    #print(f'CSV Header:{csv_header}')
-
+   
     firstrow = csv_header
     previouscandidate = firstrow[2]
     count = 0
     candidates = []
 
+    # Loop through csv file
     for row in csvreader:
-        #print(row)
+        
+        # Create a list of all candidates
         candidates += [row[2]]
+
+        # Create a list of actual candidates
         if row[2] != previouscandidate:
             candidate = row[2]
             listofvotes = [previousvotes]
@@ -32,15 +36,15 @@ with open(csvpath) as csvfile:
          
         previouscandidate = row[2]
     
+    # Loop through the list of actual candidates and count votes for each candidate and find a Winner
     for candidate in listofcandidates:
         listofvotes += [candidates.count(candidate)]
         if candidates.count(candidate) == max(listofvotes):
             winner = candidate   
     listofvotes.pop(0)
-    #print(listofcandidates)
-    #print(listofvotes)
     
     
+# Print results to terminal    
 print("Election Results")
 print("----------------------------------------------------------")
 print("Total Votes:", (csvreader.line_num - 1))
@@ -51,3 +55,17 @@ print(f'{listofcandidates[2]}: {round(((listofvotes[2]/(csvreader.line_num - 1))
 print("----------------------------------------------------------")
 print("Winner:", winner)
 print("----------------------------------------------------------")
+
+#Write results to text file
+file = open("PyPoll\Analysis\Results.txt","w")
+file.writelines(f"\n Election Results")
+file.writelines(f"\n\n ----------------------------------------------------------")
+file.writelines(f"\n\n Total Votes: {csvreader.line_num - 1}")
+file.writelines(f"\n\n ----------------------------------------------------------")
+file.writelines(f'\n\n {listofcandidates[0]}: {round(((listofvotes[0]/(csvreader.line_num - 1))*100),3)}% ({listofvotes[0]})')
+file.writelines(f'\n\n {listofcandidates[1]}: {round(((listofvotes[1]/(csvreader.line_num - 1))*100),3)}% ({listofvotes[1]})')
+file.writelines(f'\n\n {listofcandidates[2]}: {round(((listofvotes[2]/(csvreader.line_num - 1))*100),3)}% ({listofvotes[2]})')
+file.writelines(f"\n\n ----------------------------------------------------------")
+file.writelines(f"\n\n Winner: {winner}")
+file.writelines(f"\n\n ----------------------------------------------------------")
+file.close()
